d98ae1c550a08b6e0cb92619b5557a098c9f5c85
Danilo <realdanilo@users.noreply.github.com>
2021-05-27 19:25:10 -0700

    Separating and setting up  project Areas,Controllers,Actions

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BulkyBook.Data
+namespace BulkyBook.DatAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
---
+++ BulkyBook.Utility/SD.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Utility
+{
+    //add 'public static' words so can work as a reference on the main project
+    public static class SD
+    {
+    }
+}
