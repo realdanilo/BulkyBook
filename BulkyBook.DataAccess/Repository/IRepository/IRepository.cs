f28bdab1ca820781ed567423a93d0dc6a2e2a168
Danilo <realdanilo@users.noreply.github.com>
2021-06-03 18:05:06 -0700

    create common IRepository

---
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    //make interface public
+    //<T> means that accepts a generic class, needs to specify "where T:class"
+    public interface IRepository<T> where T : class
+    {
+        //finds one object by id, in T Class
+        T Get(int id);
+
+        //Gets a list of T objects
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>,IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+            );
+
+        IEnumerable<T> GetFirstOrDefault(
+         Expression<Func<T, bool>> filter = null,
+         string includeProperties = null
+         );
+
+        //entity >> object that has an id, is real(true) and needs to be created
+        void Add(T entity);
+
+        //remove by id
+        void Remove(int id);
+
+        //Remove by entity, also same as remove by id
+        void Remove(T Entity);
+
+        //remove a list/range entities
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
