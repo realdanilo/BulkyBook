f69721a7619451452da72d82b89dc323fcbb2970
Danilo <realdanilo@users.noreply.github.com>
2021-06-24 19:33:08 -0700

    configure session extension and add to start up class

--- BulkyBook/Startup.cs
+++ BulkyBook/Startup.cs
@@ -71,6 +71,13 @@ namespace BulkyBook
                 options.ClientId = "";
                 options.ClientSecret = "";
             });
+            //ading session
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -91,7 +98,7 @@ namespace BulkyBook
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseSession();
             app.UseAuthentication();
             app.UseAuthorization();
 
