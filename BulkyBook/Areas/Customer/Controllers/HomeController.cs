72695a06af7ff3c8c27702f28c6f4d041142eca3
Danilo <realdanilo@users.noreply.github.com>
2021-06-24 19:21:29 -0700

    add product details controller; shopping cart. change link from upsert to buy product.

--- BulkyBook/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBook/Areas/Customer/Controllers/HomeController.cs
@@ -1,12 +1,14 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace BulkyBook.Areas.Customer.Controllers
@@ -33,6 +35,60 @@ namespace BulkyBook.Areas.Customer.Controllers
         {
             return View();
         }
+        public IActionResult Details(int id)
+        {
+            var productFromDb = _unitOfWork.Product.GetFirstOrDefault(o => o.Id == id, includeProperties:"Category,CoverType");
+            ShoppingCart cartObj = new ShoppingCart()
+            {
+                Product = productFromDb,
+                ProductId = productFromDb.Id
+            };
+            return View(cartObj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        //user must be signed in
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            shoppingCart.Id = 0;
+            if(ModelState.IsValid)
+            {
+                //find id of logged in user
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                shoppingCart.AplicationUserId = claim.Value;
+
+                ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(n => n.AplicationUserId == shoppingCart.AplicationUserId && n.ProductId == shoppingCart.ProductId, includeProperties:"Product");
+                if(cartFromDb==null)
+                {
+                    //no records from shopping cart
+                    _unitOfWork.ShoppingCart.Add(shoppingCart);
+
+                }
+                else
+                {
+                    //update count
+                    cartFromDb.Count += shoppingCart.Count;
+                    _unitOfWork.ShoppingCart.Update(cartFromDb);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                //if model is not valid, return original product, from if
+                var productFromDb = _unitOfWork.Product.GetFirstOrDefault(o => o.Id == shoppingCart.ProductId, includeProperties: "Categories,CoverType");
+                ShoppingCart cartObj = new ShoppingCart()
+                {
+                    Product = productFromDb,
+                    ProductId = productFromDb.Id
+                };
+                return View(cartObj);
+
+            }
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
