520824eaf6599199133a92f954ae545738c4766d
Danilo <realdanilo@users.noreply.github.com>
2021-06-26 14:03:24 -0700

    add shoping cart vm and view index cartcontroller.

---
+++ BulkyBook/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,63 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IEmailSender _emailSender;
+        private readonly UserManager<IdentityUser>_userManager;
+
+        public ShoppingCartViewModel shoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork, IEmailSender emailSender, UserManager<IdentityUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _emailSender = emailSender;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            //get user id of loggedin user
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            shoppingCartVM = new ShoppingCartViewModel()
+            {
+                OrderHeader = new OrderHeader(),
+                ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.AplicationUserId == claim.Value, includeProperties:"Product")
+            };
+            shoppingCartVM.OrderHeader.OrderTotal = 0;
+            shoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == claim.Value, includeProperties:"Company");
+
+            //decrypt(get all info) items in list
+            foreach(var list in shoppingCartVM.ListCart)
+            {
+                //not mapped, so we need to calculate it
+                list.Price = SD.GetPriceBasedOnQty(list.Count, list.Product.Price, list.Product.Price50, list.Product.Price100);
+
+
+                shoppingCartVM.OrderHeader.OrderTotal += (list.Price * list.Count);
+                //bc of viewmodel, convert to raw html 
+                list.Product.Description = SD.ConvertToRawHtml(list.Product.Description);
+                //display only first 100 chars
+                if(list.Product.Description.Length > 100)
+                {
+                    list.Product.Description = list.Product.Description.Substring(0, 99) + "...";
+                }
+            }
+            return View(shoppingCartVM);
+        }
+    }
+}
