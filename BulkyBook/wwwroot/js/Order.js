7fcc28769c6357829178eea0a16bdab41302d86f
Danilo <realdanilo@users.noreply.github.com>
2021-07-07 19:00:51 -0700

    add order controller get api, set index view p1

---
+++ BulkyBook/wwwroot/js/Order.js
@@ -0,0 +1,53 @@
+﻿var dataTable;
+
+$(document).ready(() => {
+	loadDataTable();
+})
+
+function loadDataTable() {
+	dataTable = $("#tblData").DataTable({
+		"ajax": {
+			"url": "/Admin/Category/GetAll"
+		},
+		"columns": [
+			{ "data": "name", "width": "60%" },
+			{
+				"data": "id", "render": function (data) {
+					return `
+							<div class="text-center">
+								<a href="/Admin/Category/Upsert/${data}" class="btn btn-success text-white">Edit</a>
+								<a onclick=deleteCategory("/Admin/Category/Delete/${data}") class="btn btn-danger text-white">Delete</a>
+								
+							</div>
+							`}, 
+				"width":"40%"
+			}
+		]
+	})
+}
+
+function deleteCategory(url) {
+	swal({
+		title: "Are you sure you want to delete",
+		text: "you wont be able to restore the data",
+		icon: "warning",
+		buttons: true,
+		dangerMode:true 
+	}).then((res) => {
+		//if resp is true to delete
+		if (res) {
+			$.ajax({
+				type: "delete",
+				url,
+				success: function ({ success, message }) {
+					if (success) {
+						toastr.success(message)
+						dataTable.ajax.reload()
+					} else {
+						toastr.error(message)
+					}
+				}
+			})
+		}
+	})
+}
