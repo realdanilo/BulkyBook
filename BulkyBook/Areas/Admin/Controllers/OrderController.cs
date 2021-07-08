b17ebee7953ac9a54e4366ea122897470e168687
Danilo <realdanilo@users.noreply.github.com>
2021-07-07 19:24:31 -0700

    Order javascript UI, get list, switch between status

--- BulkyBook/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBook/Areas/Admin/Controllers/OrderController.cs
@@ -1,16 +1,19 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace BulkyBook.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Authorize]
+    //means someone has to log in
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -25,9 +28,49 @@ namespace BulkyBook.Areas.Admin.Controllers
 
         #region API CALLS
         [HttpGet]
-        public IActionResult GetOrderList()
+        public IActionResult GetOrderList(string status)
         {
-            IEnumerable<OrderHeader> orderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            IEnumerable<OrderHeader> orderHeaderList;
+            
+            var claimIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if(User.IsInRole(SD.Role_Admin)|| User.IsInRole(SD.Role_Employee))
+            {
+                orderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            }
+            else
+            {
+                orderHeaderList = _unitOfWork.OrderHeader.GetAll( u => u.ApplicationUserId == claim.Value,includeProperties: "ApplicationUser");
+
+            }
+            switch (status)
+            {
+                case "pending":
+                    orderHeaderList = orderHeaderList.Where(order => order.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+
+                case "completed":
+                    orderHeaderList = orderHeaderList.Where(order => order.OrderStatus == SD.StatusShipped);
+                    break;
+
+                case "rejected":
+                    orderHeaderList = orderHeaderList.Where(order => order.OrderStatus == SD.StatusCancelled ||
+                    order.OrderStatus == SD.StatusRefunded ||
+                    order.OrderStatus == SD.PaymentStatusDelayedRejected);
+                    break;
+
+                case "inprocess":
+                    orderHeaderList = orderHeaderList.Where(order => order.OrderStatus == SD.StatusApproved ||
+                    order.OrderStatus == SD.StatusInProcess||
+                    order.OrderStatus == SD.StatusPending
+                    );
+
+                    break;
+                default:
+                    break;
+
+            }
 
             return Json(new { data = orderHeaderList });
         }
--- BulkyBook/wwwroot/js/Order.js
+++ BulkyBook/wwwroot/js/Order.js
@@ -1,53 +1,38 @@
 ﻿var dataTable;
-
+var url;
 $(document).ready(() => {
 	loadDataTable();
+	url = window.location.search;
+	if (url.includes("inprocess")) { loadDataTable("GetOrderList?status=inprocess") }
+	else if (url.includes("pending")) { loadDataTable("GetOrderList?status=pending") }
+	else if (url.includes("completed")) { loadDataTable("GetOrderList?status=completed") }
+	else if (url.includes("rejected")) { loadDataTable("GetOrderList?status=rejected") }
+	else loadDataTable("GetOrderList?status=rejected") 
+
 })
 
-function loadDataTable() {
+function loadDataTable(urlGoTo) {
 	dataTable = $("#tblData").DataTable({
 		"ajax": {
-			"url": "/Admin/Category/GetAll"
+			"url": "/Admin/Order/"+urlGoTo
 		},
 		"columns": [
-			{ "data": "name", "width": "60%" },
+			{ "data": "id", "width": "10%" },
+			{ "data": "name", "width": "10%" },
+			{ "data": "phoneNumber", "width": "10%" },
+			{ "data": "applicationUser.email", "width": "10%" },
+			{ "data": "orderStatus", "width": "10%" },
+			{ "data": "orderTotal", "width": "10%" },
 			{
 				"data": "id", "render": function (data) {
 					return `
 							<div class="text-center">
-								<a href="/Admin/Category/Upsert/${data}" class="btn btn-success text-white">Edit</a>
-								<a onclick=deleteCategory("/Admin/Category/Delete/${data}") class="btn btn-danger text-white">Delete</a>
-								
+								<a href="/Admin/Order/Details/${data}" class="btn btn-success text-white">Edit</a>
 							</div>
 							`}, 
-				"width":"40%"
+				"width":"5%"
 			}
 		]
 	})
 }
 
-function deleteCategory(url) {
-	swal({
-		title: "Are you sure you want to delete",
-		text: "you wont be able to restore the data",
-		icon: "warning",
-		buttons: true,
-		dangerMode:true 
-	}).then((res) => {
-		//if resp is true to delete
-		if (res) {
-			$.ajax({
-				type: "delete",
-				url,
-				success: function ({ success, message }) {
-					if (success) {
-						toastr.success(message)
-						dataTable.ajax.reload()
-					} else {
-						toastr.error(message)
-					}
-				}
-			})
-		}
-	})
-}
